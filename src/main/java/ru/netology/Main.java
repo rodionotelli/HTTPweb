b876107ec888f987916e15d347b8d5227291a6d4
Kirill <rodionoff2010@gmail.com>
2024-09-24 22:18:21 +0300

    refactoring code

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,47 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         final var server = new Server();
-        server.start();
+
+        server.addHandler("GET", "/classic.html", new Handler() {
+                    public void handle(Request request, BufferedOutputStream out) throws IOException {
+                        try {
+                            final var filePath = Path.of(".", "public", request.getPath());
+                            final var mimeType = Files.probeContentType(filePath);
+                            System.out.println(request.getPath());
+                            if (request.getQueryParams() != null) {
+                                request.getQueryParams().forEach((key, value) -> System.out.println(key + ":" + value));
+                            }
+                            // special case for classic
+                            final var template = Files.readString(filePath);
+                            final var content = template.replace(
+                                    "{time}",
+                                    LocalDateTime.now().toString()
+                            ).getBytes();
+                            out.write((
+                                    "HTTP/1.1 200 OK\r\n" +
+                                            "Content-Type: " + mimeType + "\r\n" +
+                                            "Content-Length: " + content.length + "\r\n" +
+                                            "Connection: close\r\n" +
+                                            "\r\n"
+                            ).getBytes());
+                            out.write(content);
+                            out.flush();
+
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                }
+        );
+        server.listen(9999);
     }
 }
 
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,104 +1,83 @@
 package ru.netology;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class Server {
 
-    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
-            "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html",
-            "/classic.html", "/events.html", "/events.js");
     private final ExecutorService executorService = Executors.newFixedThreadPool(64);
-    private static final int PORT = 9999;
-    private static final int LIMIT_CONNECTION = 64;
+    private Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+
 
     public Server() {
 
     }
 
-    public void start() {
-        try (final ServerSocket serverSocket = new ServerSocket(PORT)) {
+    public void listen(int port) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 final var socket = serverSocket.accept();
-                executorService.submit(() -> connect(socket));
+                executorService.submit(() -> {
+                    try {
+                        connect(socket);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 
-
-    public void connect(Socket socket) {
+    private void connect(Socket socket) throws IOException, URISyntaxException {
         try (
                 socket;
-                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final var out = new BufferedOutputStream(socket.getOutputStream())) {
-            final String[] parts = getResponse(in);
-            final var path = parts[1];
-
-            if (parts.length != 3) {
-                // just close socket
-            } else if (!validPaths.contains(path)) {
-                errorResponse(out);
-            } else {
-                getTrueResponse(out, path);
+                final var in = socket.getInputStream();
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            var request = Request.getRequest(in, out);
+            var handlerMap = handlers.get(request.getMethod());
+            if (handlerMap == null) {
+                notFoundError(out);
+                return;
             }
+            var handler = handlerMap.get(request.getPath());
+            if (handler == null) {
+                notFoundError(out);
+                return;
+            }
+            handler.handle(request, out);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String[] getResponse(BufferedReader in) throws IOException {
-        final var requestLine = in.readLine();
-        final var parts = requestLine.split(" ");
-        return parts;
-    }
-
-    public void errorResponse(BufferedOutputStream out) throws IOException {
-        String response = "HTTP/1.1 404 Not Found\r\n" +
-                "Content-Length: 0\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
-        out.write(response.getBytes());
+    private void notFoundError(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
         out.flush();
     }
 
-    public String correctResponse(String mimeType, long length) {
-        return "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: " + mimeType + "\r\n" +
-                "Content-Length: " + length + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
-    }
-
-    public void getTrueResponse(BufferedOutputStream out, String path) throws IOException {
-        final var filePath = Path.of(".", "public", path);
-        final var mimeType = Files.probeContentType(filePath);
-
-        if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
-            out.write(correctResponse(mimeType, content.length).getBytes());
-            out.write(content);
-        } else {
-            final var length = Files.size(filePath);
-            out.write(correctResponse(mimeType, length).getBytes());
-            Files.copy(filePath, out);
+    public void addHandler(String method, String path, Handler handler) {
+        if (handlers.get(method) == null) {
+            handlers.put(method, new ConcurrentHashMap<>());
         }
-        out.flush();
+        handlers.get(method).put(path, handler);
     }
 }
